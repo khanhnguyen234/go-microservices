db6ca496e6e2f7238611cb7cbac844827d1e7942
EC2 Default User <ec2-user@ip-172-31-16-40.ap-southeast-1.compute.internal>
2020-09-27 17:35:51 +0000

    cmt to work

--- product-service/go.mod
+++ product-service/go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/fatih/color v1.9.0
+	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-redis/redis/v7 v7.4.0
 	github.com/jinzhu/gorm v1.9.16
--- product-service/main.go
+++ product-service/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"khanhnguyen234/product-service/_elastic"
-	"khanhnguyen234/product-service/_mongo"
-	"khanhnguyen234/product-service/_rabbitmq"
-	"khanhnguyen234/product-service/_redis"
+	//"khanhnguyen234/product-service/_elastic"
+	//"khanhnguyen234/product-service/_mongo"
+	//"khanhnguyen234/product-service/_rabbitmq"
+	//"khanhnguyen234/product-service/_redis"
 	"khanhnguyen234/product-service/apis/product"
 	"khanhnguyen234/product-service/common"
 	"os"
@@ -17,16 +18,16 @@ func main() {
 	err := godotenv.Load()
 	common.LogStatus(err, "Load Env")
 
-	_mongo.ConnectMongo()
-	_redis.ConnectRedis()
-	_elastic.ConnectElastic()
-	_rabbitmq.ConnectRabbitMQ()
+	//_mongo.ConnectMongo()
+	//_redis.ConnectRedis()
+	//_elastic.ConnectElastic()
+	//_rabbitmq.ConnectRabbitMQ()
 
 	//gin.SetMode(gin.ReleaseMode)
 	route := gin.Default()
 
 	product.ProductRouters(route.Group("/product"))
-	product.CreateProductSub()
-
+	//product.CreateProductSub()
+	route.Use(cors.Default())
 	route.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
 }
